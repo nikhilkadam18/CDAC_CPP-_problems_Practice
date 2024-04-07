cc43f7e812382f40e24202b926f980a1233acb14
Nikhil Kadam <83809232nk@gmail.com>
2024-04-07 13:35:33 +0530

    new changes add diagonal sum logic

--- 2d_array_diagonal_sum.cpp
+++ 2d_array_diagonal_sum.cpp
@@ -3,24 +3,33 @@ using namespace std;
 
 int main()
 {
-	int i,j;
+	int i,j,sum=0,row,col;
 	int s[3][3];
-	cout<<"enter numbers:"<<endl;
-	for(i=0;i<3;i++)
+	cout<<"enter number of rows: "<<endl;
+	cin>>row;
+	cout<<"enter number of col: "<<endl;
+	cin>>col;
+	cout<<"enter numbers for matrix:"<<endl;
+	for(i=0;i<row;i++)
 	{
-		for(j=0;j<3;j++)
+		for(j=0;j<col;j++)
 		{
 			cin>>s[i][j];
 		}
 	}
 	cout<<"matrix is:"<<endl;
-	for(i=0;i<3;i++)
+	for(i=0;i<row;i++)
 	{
-		for(j=0;j<3;j++)
+		for(j=0;j<col;j++)
 		{
+			if(i==j)
+			{
+				sum=sum+s[i][j];
+			}
 			cout<<" "<<s[i][j];
 		}
 		cout<<endl;
 	}
+	cout<<"Diagonal sum is: "<<sum<<endl;
 	
 }
